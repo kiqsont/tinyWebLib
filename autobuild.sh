779d1acaf3d9fe33755fdfff18cde10633607218
kiqsont <1537788688@qq.com>
2023-02-11 11:47:55 +0800

    build script

--- autobuild.sh
+++ autobuild.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 rm -rf build 
+rm -rf asyncLogger/build
+rm lib/*
+rm example/bin/*
+
+cmake asyncLogger/ -B asyncLogger/build/
+make --directory=asyncLogger/build/
+
 cmake -B build
 cmake --build build
+
+cp asyncLogger/lib/libasyncLogger.so lib/
